1f9907f745ca2049e53cc5bc98a7a1110ce8a392
Albert Lin <yoyj1314@gmail.com>
2025-09-05 20:53:18 -0700

    Exploning Visual Studio
    Basic project with an extra lin

---
+++ GD14_1133_Lab1_Yaushen_Lin/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_Yaushen_Lin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Albert ENTA 1333");
+        }
+    }
+}
