2997ef468925c445417bdf85c6d36031a4afb414
Albert Lin <yoyj1314@gmail.com>
2025-09-08 15:31:55 -0700

    Update Program.cs

--- GD14_1133_Lab1_Yaushen_Lin/Program.cs
+++ GD14_1133_Lab1_Yaushen_Lin/Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Albert ENTA 1333");
+            Console.WriteLine("Hello, World! Albert");
+            Console.WriteLine("Decimal: 0 Binary: 00000000 Hex: 0");
+            Console.WriteLine("Decimal: 1 Binary: 00000001 Hex: 1");
+            Console.WriteLine("Decimal: 2 Binary: 00000010 Hex: 2");
+            Console.WriteLine("Decimal: 4 Binary: 00000100 Hex: 4");
+            Console.WriteLine("Decimal: 8 Binary: 00001000 Hex: 8");
+            Console.WriteLine("Decimal: 16 Binary: 00010000 Hex: 10");
+            Console.WriteLine("Decimal: 32 Binary: 00100000 Hex: 20");
+            Console.WriteLine("Decimal: 64 Binary: 01000000 Hex: 40");
+            Console.WriteLine("Decimal: 100 Binary: 01100100 Hex: 64");
+            Console.WriteLine("Decimal: 255 Binary: 11111111 Hex: ff");
+            Console.WriteLine("Good Bye date: 2025.09.08");
         }
     }
 }
